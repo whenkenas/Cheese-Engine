ce32ce4a180c31af1b867c82d8ed2a4eec2b72eb
chris <84947968+whenkenas@users.noreply.github.com>
2026-05-19 22:46:26 -0400

    new version 1.45

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -17,7 +17,7 @@ enum MainMenuColumn {
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '1.0.4'; // This is also used for Discord RPC
-	public static var cheeseEngineVersion:String = '1.4';
+	public static var cheeseEngineVersion:String = '1.45';
 	public static var curSelected:Int = 0;
 	public static var curColumn:MainMenuColumn = CENTER;
 	var allowMouse:Bool = true; //Turn this off to block mouse movement in menus
